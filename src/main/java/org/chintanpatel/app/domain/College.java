25d087de7e1df699d9f6c5ef9b01014c6e7c4808
chintanpatel <chintanpatel2013@icloud.com>
2023-12-29 17:58:27 +0530

    Add College.java entity file for application.

---
+++ src/main/java/org/chintanpatel/app/domain/College.java
@@ -0,0 +1,26 @@
+package org.chintanpatel.app.domain;
+
+public class College {
+
+    private int collegeId;
+    private String collegeName;
+
+    public College() {
+    }
+
+    public int getCollegeId() {
+        return collegeId;
+    }
+
+    public void setCollegeId(int collegeId) {
+        this.collegeId = collegeId;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+}

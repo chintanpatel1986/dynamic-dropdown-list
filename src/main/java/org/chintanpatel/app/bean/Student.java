a6b546f968ccba468a1acdf4f2ee004e9f645052
chintanpatel <chintanpatel2013@icloud.com>
2024-01-23 14:58:20 +0530

    Add Student.java file for crud application.

---
+++ src/main/java/org/chintanpatel/app/bean/Student.java
@@ -0,0 +1,93 @@
+package org.chintanpatel.app.bean;
+
+import java.util.Date;
+
+public class Student {
+
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long mobile;
+    private Date birthDate;
+    private String userName;
+    private String password;
+
+    public Student() {
+    }
+
+    public Student(int studentId, String firstName, String lastName, String email, long mobile, Date birthDate, String userName, String password) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobile = mobile;
+        this.birthDate = birthDate;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

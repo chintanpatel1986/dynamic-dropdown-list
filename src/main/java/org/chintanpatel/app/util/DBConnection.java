5450a872a80f92de9586d9359c0ba39f9ce51603
chintanpatel <chintanpatel2013@icloud.com>
2024-01-23 12:23:23 +0530

    Create project and add DBConnection.java file for crud application.

---
+++ src/main/java/org/chintanpatel/app/util/DBConnection.java
@@ -0,0 +1,29 @@
+package org.chintanpatel.app.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String USER = "chintanpatel";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String DRIVER = "org.postgresql.Driver";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+                if (connection == null) {
+                    System.out.println("Can not connect to database.");
+                } else {
+                    System.out.println("Connect to database.");
+                }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
